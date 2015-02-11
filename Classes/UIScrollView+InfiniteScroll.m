848d4e072b4f7cbe12c488190515fe1f553a9226
Andrej Mihajlov <and@codeispoetry.ru>
2015-02-11 21:37:06 +0100

    Accommodate bottom bar on scroll to bottom

--- Classes/UIScrollView+InfiniteScroll.m
+++ Classes/UIScrollView+InfiniteScroll.m
@@ -351,7 +351,7 @@ CGFloat pb_infiniteScrollExtraBottomInset;
         // Initiate scroll to the bottom if due to user interaction contentOffset.y
         // stuck somewhere between last cell and activity indicator
         if(finished) {
-            CGFloat newY = self.contentSize.height - self.bounds.size.height;
+            CGFloat newY = self.contentSize.height - self.bounds.size.height + self.contentInset.bottom;
             
             if(self.contentOffset.y > newY && newY > 0) {
                 [self setContentOffset:CGPointMake(0, newY) animated:YES];
