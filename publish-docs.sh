d41a54384f80d7f2d965a4a8a3098476f4cfb24a
Andrej Mihajlov <and@codeispoetry.ru>
2016-12-20 18:27:54 +0000

    Force push docs subtree to gh-pages

--- publish-docs.sh
+++ publish-docs.sh
@@ -15,6 +15,6 @@ jazzy \
   --module UIScrollView_InfiniteScroll
 
 git add docs && git commit -m "Update docs subtree commit"
-git subtree push --prefix docs origin gh-pages
+git push origin `git subtree split --prefix docs master`:gh-pages --force
 git reset --soft HEAD~1
 git rm -rf docs
