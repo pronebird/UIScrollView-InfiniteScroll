54e862b21e76fa90b398e6af2af9bf140ecee7aa
Andrej Mihajlov <and.mikhaylov@gmail.com>
2018-04-15 17:34:31 +0200

    Update documentation script

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-jazzy \
-  --clean \
-  --objc \
-  --sdk iphoneos \
-  --theme fullwidth \
-  --author 'Andrei Mihailov' \
-  --author_url http://codeispoetry.ru/ \
-  --github_url https://github.com/pronebird/UIScrollView-InfiniteScroll \
-  --github-file-prefix https://github.com/pronebird/UIScrollView-InfiniteScroll/tree/1.0.0 \
-  --module-version 1.0.0 \
-  --umbrella-header Classes/UIScrollView+InfiniteScroll.h \
-  --framework-root Classes \
-  --module UIScrollView_InfiniteScroll
+jazzy --config .jazzy.yml
 
 git add docs && git commit -m "Update docs subtree commit"
 git push origin `git subtree split --prefix docs master`:gh-pages --force
