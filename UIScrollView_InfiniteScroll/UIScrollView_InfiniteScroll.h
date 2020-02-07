53d88dba5bb2d666f6c4638fc7fd483fd7bd747a
Damian Kolasinski <damian.kolasinski@appunite.com>
2020-02-07 13:59:15 +0100

    Added Carthage support

---
+++ UIScrollView_InfiniteScroll/UIScrollView_InfiniteScroll.h
@@ -0,0 +1,15 @@
+//
+//  UIScrollView_InfiniteScroll.h
+//  UIScrollView_InfiniteScroll
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIScrollView_InfiniteScroll.
+FOUNDATION_EXPORT double UIScrollView_InfiniteScrollVersionNumber;
+
+//! Project version string for UIScrollView_InfiniteScroll.
+FOUNDATION_EXPORT const unsigned char UIScrollView_InfiniteScrollVersionString[];
+
+
+#import "UIScrollView+InfiniteScroll.h"
